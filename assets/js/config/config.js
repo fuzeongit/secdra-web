7daba4b1e07d369bdb77fe88ad8cc50ed254d99d
fjj <245142637@qq.com>
2018-11-02 20:58:24 +0800

    .

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -4,6 +4,7 @@ export default {
 
 
 function get (){
-  console.log( process.env.NODE_ENV === "development" ? "/api" : "http://47.107.131.24/api");
-  return process.env.NODE_ENV === "development" ? "/api" : "http://47.107.131.24/api"
+  // console.log( process.env.NODE_ENV === "development" ? "/api" : "http://47.107.131.24/api");
+  // return process.env.NODE_ENV === "development" ? "/api" : "http://47.107.131.24/api"
+  return "http://47.107.131.24/api"
 }
