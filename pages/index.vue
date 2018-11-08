366bbd4d0e781cf3ba2cca48b9e03fb58c1a7f92
fjj <245142637@qq.com>
2018-11-08 16:52:16 +0800

    。

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="page">
     <div class="content row">
-      <div class="card image-card">
-
+      <div class="left-box">
+        <div class="card image-card row">
+          <div v-for="(draw,index) in likeList" class="item" :key="index">
+            <div class="cover img" :style="{backgroundImage: `url(${$img.scedra(draw.url,'specifiedWidth')})`}"
+                 style="width: 100%">
+            </div>
+            <p>
+              {{draw.user.name}}
+            </p>
+          </div>
+        </div>
+        <div class="card image-card row" style="margin-top: 20px;">
+          <div v-for="(draw,index) in newList" class="item" :key="index">
+            <div class="cover img" :style="{backgroundImage: `url(${$img.scedra(draw.url,'specifiedWidth')})`}"
+                 style="width: 100%">
+            </div>
+            <p>
+              {{draw.user.name}}
+            </p>
+          </div>
+        </div>
       </div>
       <!--<div class="card tag-card">-->
       <!--<div class="tag-first">-->
@@ -14,15 +33,19 @@
       <!--</div>-->
       <!--</div>-->
       <!--</div>-->
-      <div class="card tag-card">
-        <h3 class="title">
-          推荐标签
-          <nuxt-link to="/">
-            更多>>
-          </nuxt-link>
-        </h3>
-        <div class="tag-list">
-          <nuxt-link class="btn is-plain" v-for="(tag,index) in tagList" :to="`/draw/search/${tag.name}`" :key="index"> {{tag.name}}</nuxt-link>
+      <div class="right-box">
+        <div class="card tag-card">
+          <h3 class="title">
+            推荐标签
+            <nuxt-link to="/">
+              更多>>
+            </nuxt-link>
+          </h3>
+          <div class="tag-list">
+            <nuxt-link class="btn is-plain" v-for="(tag,index) in tagList" :to="`/draw/search/${tag.name}`"
+                       :key="index"> {{tag.name}}
+            </nuxt-link>
+          </div>
         </div>
       </div>
     </div>
@@ -31,15 +54,21 @@
 
 <script>
   import config from "../assets/js/config";
+  import {Pageable} from "../assets/js/model/base";
 
   export default {
     //在这里不能使用httpUtil
     async asyncData({store, req, redirect, route, $axios}) {
       store.state.menu.name = "home";
-      let res = await $axios.get(`${config.host}/tag/listTagOrderByLikeAmount`);
-      let result = res.data || {};
+      let taskList = [];
+      taskList.push($axios.get(`${config.host}/tag/listTagOrderByLikeAmount`));
+      taskList.push($axios.get(`${config.host}/draw/pagingByTag`, {params:  new Pageable(0,10,"likeAmount,desc")}));
+      taskList.push($axios.get(`${config.host}/draw/pagingByTag`, {params: new Pageable(0,10,"createDate,desc")}));
+      let resultList = (await Promise.all(taskList)).map(item => item.data);
       return {
-        tagList: result.data
+        tagList: resultList[0].data,
+        likeList: resultList[1].data.content,
+        newList: resultList[2].data.content,
       }
     },
     data() {
@@ -52,9 +81,7 @@
     mounted() {
 
     },
-    methods: {
-
-    }
+    methods: {}
   }
 </script>
 
@@ -64,17 +91,62 @@
 
   .page {
     padding: 30px 0;
-
   }
 
   .content {
     width: @visual-width;
     margin: 0 auto;
-    .image-card {
-      width: 750px;
-      margin-right: 20px;
+    .left-box {
+      width: 850px;
       float: left;
+      .activity-card {
+
+      }
+      .image-card {
+        padding: 16px 9px;
+        .item {
+          margin: 8px;
+          float: left;
+          width: 150px;
+          .cover {
+            width: 100%;
+            height: 150px;
+          }
+        }
+      }
     }
+    .right-box {
+      width: 250px;
+      float: right;
+      .tag-card {
+        @spacing: 10px;
+        padding: @spacing;
+        .title {
+          font-size: @medium-font-size;
+          padding-left: 10px;
+          line-height: @medium-font-size;
+          border-left: 5px solid @theme-color;
+          a {
+            line-height: 16px;
+            vertical-align: baseline;
+            display: inline-block;
+            float: right;
+            color: @theme-color
+          }
+        }
+        .tag-list {
+          margin-top: @spacing * 2;
+          margin-bottom: -@spacing;
+          .btn {
+            margin-right: @spacing;
+            margin-bottom: @spacing;
+            line-height: 25px;
+            padding: 0 2em;
+          }
+        }
+      }
+    }
+
     /*  .tag-card {
         @spacing:10px;
          width: 250px;
@@ -104,49 +176,8 @@
          }
     }*/
 
-    .tag-card {
-      @spacing: 10px;
-      width: 250px;
-      float: left;
-      padding: @spacing;
-      .title {
-        font-size: @medium-font-size;
-        padding-left: 10px;
-        line-height: @medium-font-size;
-        border-left: 5px solid @theme-color;
-        a{
-          line-height: 16px;
-          vertical-align: baseline;
-          display: inline-block;
-          float: right;
-          color:@theme-color
-        }
-      }
-      .tag-list {
-        margin-top: @spacing * 2;
-        margin-bottom: -@spacing;
-        .btn {
-          margin-right: @spacing;
-          margin-bottom: @spacing;
-          line-height: 25px;
-          padding: 0 2em;
-        }
-      }
-    }
   }
 </style>
 <style>
 
 </style>
-
-<!--i*w-->
-<!--i % col * w = left;-->
-<!--(i/col)-->
-<!--0-3:0-->
-<!--4-7:1-->
-
-<!--0-0-->
-<!--1-250-->
-<!--2-500-->
-<!--3-750-->
-<!--4-0-->
