f03e81818459c5f8ea0310db56a4aaa923d500a0
fjj <245142637@qq.com>
2018-11-12 17:46:26 +0800

    .

--- assets/js/config/index.js
+++ assets/js/config/index.js
@@ -1,6 +1,6 @@
 export default {
   // host: process.env.NODE_ENV === "development" ? "/api" : "http://47.107.131.24/api",
-  host: process.env.NODE_ENV === "development" ? "/api" : "http://47.107.181.201//api",
+  host: process.env.NODE_ENV === "development" ? "/api" : "http://47.107.181.201/api",
   qiniu: process.env.NODE_ENV === "development" ? "http://ph9jy186h.bkt.clouddn.com" : "http://ph9jy186h.bkt.clouddn.com",
   qiniuHead: process.env.NODE_ENV === "development" ? "http://phnqizf7z.bkt.clouddn.com" : "http://phnqizf7z.bkt.clouddn.com",
   qiniuBack: process.env.NODE_ENV === "development" ? "http://phnqlj10o.bkt.clouddn.com" : "http://phnqlj10o.bkt.clouddn.com",
