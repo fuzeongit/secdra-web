63dace686a380f30b1dc2e95f6520ddc01c9b240
fjj <245142637@qq.com>
2019-08-28 16:20:23 +0800

    为免登陆做准备

--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import vuex from "vuex"
-
+// import { Result } from "../assets/script/model"
+// const cookieParser = process.server ? require("cookieparser") : undefined
 /**
  * vuex仓库入口
  *
@@ -11,6 +12,21 @@ Vue.use(vuex)
 //  暂时关闭严格模式
 export const strict = false
 export const actions = {
+  // async nuxtServerInit({ commit }, { req, $axios }) {
+  //   let result = {}
+  //   let token = null
+  //   try {
+  //     const responses = await $axios.get(`/user/getInfo`)
+  //     result = responses.data
+  //     token = cookieParser.parse(req.headers.cookie).token
+  //   } catch (e) {
+  //     result = new Result(401, e, "请登录")
+  //   }
+  //   if (result.status === 200) {
+  //     commit("user/MSetUserInfo", result.data)
+  //     commit("user/MSetToken", token)
+  //   }
+  // }
   nuxtServerInit({ commit }) {
     commit("user/MSetUserInfo", null)
   }
